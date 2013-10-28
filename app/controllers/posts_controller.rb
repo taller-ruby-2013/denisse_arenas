e3958f7c5d79a57e365ffad8c5f9e77a9cc8055d
Denisse <denisse@Optimus.(none)>
2013-10-28 18:56:12 -0300

    creamos la BD, agregamos cosas a esta y creamos una vista con los datos de ls BD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,27 @@
 class PostsController < ApplicationController
+	def index
+		@post = Post.all
+	end
+	
 	def new
 	end
+
 	def create
-		render text: params[:post].inspect
+		@post = Post.new(post_params)
+
+		@post.save
+		redirect_to @post
+
+		#render text: params[:post].inspect
 	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title, :text)
+		
+		end
 end
